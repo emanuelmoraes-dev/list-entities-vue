9d09e1d57434b1f53fab32a86003b6ab901634a4
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-06-27 19:56:11 -0300

    nome de arquivos resolvidos na exportação

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import compModal from './components/vuestic-modal/VuesticModal.vue'
 import compWidget from './components/vuestic-widget/VuesticWidget.vue'
 import compSimpleSelect from './components/vuestic-simple-select/VuesticSimpleSelect.vue'
 import compVuesticScrollbar from './components/vuestic-scrollbar/VuesticScrollbar.vue'
-import directVuesticDropDown from './directives/vuestic-dropdown/VuesticDropDown'
+import directVuesticDropdown from './directives/vuestic-dropdown/VuesticDropdown'
 
 export const ListEntities = compListEntities
 export const ModalEntity = compModalEntity
@@ -12,4 +12,4 @@ export const Modal = compModal
 export const Widget = compWidget
 export const SimpleSelect = compSimpleSelect
 export const VuesticScrollbar = compVuesticScrollbar
-export const VuesticDropDown = directVuesticDropDown
+export const VuesticDropdown = directVuesticDropdown
