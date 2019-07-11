6f37c00c1802a78cb401bf07ac64955fa3b1eb99
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-07-11 00:11:49 -0300

    componentes renomeados

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import compListEntities from './components/list-entities/ListEntities.vue'
 import compModalEntity from './components/modal-entity/ModalEntity.vue'
-import compModal from './components/vuestic-modal/VuesticModal.vue'
-import compWidget from './components/vuestic-widget/VuesticWidget.vue'
-import compSimpleSelect from './components/vuestic-simple-select/VuesticSimpleSelect.vue'
+import compVuesticModal from './components/vuestic-modal/VuesticModal.vue'
+import compVuesticWidget from './components/vuestic-widget/VuesticWidget.vue'
+import compVuesticSimpleSelect from './components/vuestic-simple-select/VuesticSimpleSelect.vue'
 import compVuesticScrollbar from './components/vuestic-scrollbar/VuesticScrollbar.vue'
 import directVuesticDropdown from './directives/vuestic-dropdown/VuesticDropdown'
 
 export const ListEntities = compListEntities
 export const ModalEntity = compModalEntity
-export const Modal = compModal
-export const Widget = compWidget
-export const SimpleSelect = compSimpleSelect
+export const VuesticModal = compVuesticModal
+export const VuesticWidget = compVuesticWidget
+export const VuesticSimpleSelect = compVuesticSimpleSelect
 export const VuesticScrollbar = compVuesticScrollbar
 export const VuesticDropdown = directVuesticDropdown
