20ac1a6ea63721a6d2007ec337148d2839a171f4
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-09-01 17:17:46 -0300

    ambiente reconfigurado para vue-cli e eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-    node: true,
-    mocha: true
-  },
-  "globals": {
-    "expect": true
-  },
-  extends: [
-    'plugin:vue/recommended',
-    'standard'
-  ],
-  plugins: [
-    'vue'
-  ],
-  rules: {
-    'generator-star-spacing': 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	'extends': [
+		'plugin:vue/essential',
+		'@vue/standard'
+	],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'strict': process.env.NODE_ENV === 'production' ? 'off' : 'error',
+		'linebreak-style': process.env.NODE_ENV === 'production' ? 'off' : 'error',
+		'quotes': process.env.NODE_ENV === 'production' ? 'off' : ['error', 'single'],
+		'spaced-comment': process.env.NODE_ENV === 'production' ? 'off' : ['error', 'always'],
+		'space-before-blocks': process.env.NODE_ENV === 'production' ? 'off' : ['error', 'always'],
+		'indent': process.env.NODE_ENV === 'production' ? 'off' : ['error', 'tab'],
+		'indent-size': process.env.NODE_ENV === 'production' ? 'off' : [true, 2],
+		'no-tabs': 0
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # list-entities-vue
 
-Generic components in Vue JS to view and list entities
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
