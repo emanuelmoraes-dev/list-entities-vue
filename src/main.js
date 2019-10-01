fb554338459e0da34422e5e3f00b0d6300f1c89f
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-10-01 08:45:56 -0300

    ordem de importação em arquivo main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import '../lib/fontawesome/css/all.min.css' // css da lib de ícones 'fontawesome'
 import 'bootstrap-vue/dist/bootstrap-vue.css' // css da lib 'bootstrap-vue'
 import '../assets/css/vuestic.css' // css do template vuestic encapsulada pela classe 'wrapper-list-entities-vue'
 import '../assets/css/style.css' // definições globais de css com  o wrapper 'wrapper-list-entities-vue'
+import '../lib/fontawesome/css/all.min.css' // css da lib de ícones 'fontawesome'
 
 import Vue from 'vue'
 import app from './App.vue'
