b61ed34e250ce82da5aa83fca5072015ca9608cd
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-10-14 12:48:39 -0300

    renamed definitionAdapter to def; install updated

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import _listEntities from './src/components/list-entities/list-entities.vue'
 import _modalEntity from './src/components/modal-entity/modal-entity.vue'
-import _definitionAdapter from './src/adapters/definition-adapter'
+import _def from './src/adapters/def'
 
 export default {
-	install (Vue, { listEntitiesIsGlobal = true, modalEntityIsGlobal = true }) {
+	install (Vue, {
+		listEntitiesIsGlobal = true,
+		modalEntityIsGlobal = true,
+		nameCtx='$listEntities',
+		defCtx='def'
+	} = {}) {
 		if (listEntitiesIsGlobal)
 			Vue.component('list-entities', _listEntities)
 		if (modalEntityIsGlobal)
 			Vue.component('modal-entity', _modalEntity)
+		if (nameCtx) {
+			Vue.prototype[nameCtx] = {}
+			if (defCtx) Vue.prototype[nameCtx][defCtx] = _def
+		}
 	}
 }
 
 export const listEntities = _listEntities
 export const modalEntity = _modalEntity
-export const definitionAdapter = _definitionAdapter
+export const def = _def
