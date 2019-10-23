0812f39ba45b8b05b7c38e09d7b1555735f31db9
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-10-23 16:38:02 -0300

    bootstrap e jquery com dependencia de dev

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import 'bootstrap-vue/dist/bootstrap-vue.css' // css da lib 'bootstrap-vue'
 import '../lib/vuestic/css/vuestic.css' // css do template vuestic encapsulada pela classe 'wrapper-list-entities-vue'
 import '../lib/fontawesome/css/all.min.css' // css da lib de ícones 'fontawesome'
 import '../assets/css/style.css' // definições globais de css com  o wrapper 'wrapper-list-entities-vue'
+import jQuery from 'jquery'
+import 'bootstrap' // lib para css
+import 'bootstrap-vue/dist/bootstrap-vue.css' // css da lib 'bootstrap-vue'
 
 import Vue from 'vue'
 import app from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import plugin from '../index'
+
+global.jQuery = jQuery
+global.$ = jQuery
 
 Vue.use(plugin)
 Vue.use(BootstrapVue)
