125f4cd35d0198410a3f33fed0ada527ee7b48c4
Emanuel Moraes <emanuelmoraes297@gmail.com>
2021-05-24 00:53:42 -0300

    renamed component ListEntitiesVue to ListEntities

--- tests/unit/components/list-entities.spec.ts
+++ tests/unit/components/list-entities.spec.ts
@@ -1,7 +1,7 @@
-import ListEntitiesVue from '@/components/list-entities-vue/ListEntitiesVue.vue'
+import ListEntitiesVue from '@/components/list-entities/ListEntities.vue'
 import { shallowMount } from '@vue/test-utils'
 
-describe('components/list-entities-vue', () => {
+describe('components/list-entities', () => {
 	it('needs to have one .lev-list-entities', () => {
 		const component = shallowMount(ListEntitiesVue)
 		expect(component.findAll('.lev-list-entities').length).toBe(1)
