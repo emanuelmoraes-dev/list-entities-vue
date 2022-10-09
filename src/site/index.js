b56c7a50663a957db5f757a6fa563dba3ab17c20
Emanuel Moraes de Almeida <emanuelmoraes297@gmail.com>
2022-10-09 13:07:43 -0300

    add Hot Module Replacement

--- src/site/index.js
+++ src/site/index.js
@@ -2,3 +2,12 @@ import { createApp } from 'vue'
 import Site from './Site.vue'
 
 createApp(Site).mount('#web')
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
+    import.meta.hot.accept();
+    import.meta.hot.dispose(() => {
+        app.unmount();
+    });
+}
